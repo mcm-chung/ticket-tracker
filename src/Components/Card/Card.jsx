a2e63b3758e435a51e1f67fe29fa7e4a2f4d8c21
esmeebarr <esmeebarr@btinternet.com>
2021-06-07 17:01:42 +0100

    Added function

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,4 +2,20 @@ import React, { useState } from "react";
 import styles from "./Card.module.scss";
 import data from "../data/teams";
 
+const Card = (props) => {
+  const cardMap = team.map()
+  const cardMap = [
+    { id } { name }, { role }
+  ];
+
+  return (
+    <div className="cards">
+      {cardMap.map() => (
+
+      )}
+    </div>
+  )
+  
+
+};
 export default Card;
