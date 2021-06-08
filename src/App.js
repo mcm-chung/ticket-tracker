63939d2da2f9c0303614ef2a0e7b46ba7b87bb05
esmeebarr <esmeebarr@btinternet.com>
2021-06-08 16:52:22 +0100

    Started styling card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import { useState } from "react";
 import Card from "./Components/Card";
-import data from "./Components/data";
-import styles from "";
+import Data from "./Components/data/teams";
 
-function App() {
-  return <div className="App"></div>;
-}
+// Map over the data file below to create the components
+// Pass data into them via props
+// Use State to handle the increment and decrement of tickets.
+// Style it.
+
+const App = () => {
+  const [name, role, score, setCardScore] = useState();
+
+  return (
+    <div className="App">
+      <Card
+        cardName={name}
+        cardRole={role}
+        score={score}
+        handleClick={setCardScore}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
-import styles from "./Card.module.scss";
+import styles from "./Card.css";
 import data from "../data/teams";
 
 const Card = (props) => {
-  const cardMap = team.map()
-  const cardMap = [
-    { id } { name }, { role }
-  ];
-
   return (
-    <div className="cards">
-      {cardMap.map() => (
-
-      )}
-    </div>
-  )
-  
-
+    <section className="container">
+      <div className="headings">
+        <h2>Name{props.cardName}</h2>
+        <h2>Role{props.cardRole}</h2>
+      </div>
+      <div className="box">
+        <h4>Counter{props.score}</h4>
+        <h5>0</h5>
+        <div className="buttons">
+          <button onClick={() => props.handleClick(props.score - 1)}>-</button>
+          <button onClick={() => props.handleClick(props.score + 1)}>+</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Card;
